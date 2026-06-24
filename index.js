7d196f17b9bba37b787b500316281e3676beae56
Soham Ghodke <sohamghodke.2006@gmail.com>
2026-06-24 15:35:22 +0530

    New Commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000;
 const path = require("path");
 const { v4 : uuidv4 } = require("uuid");
 const methodOverride = require("method-override");
+const mysql = require("mysql2");
 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended : true}));
@@ -12,6 +13,13 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "quora",
+    password: "soham2006"
+})
 
 
 let posts = [
@@ -32,20 +40,55 @@ let posts = [
     }
 ];
 
-
-app.get("/posts", (req, res) => {
-    res.render("index.ejs", { posts });
+// All Posts
+app.get("/posts", async (req, res) => {
+    let q = "SELECT * FROM post";
+    try {
+        connection.query(q, (err, posts) => {
+        if(err) throw err;
+        res.render("index.ejs", { posts });
+    });
+    } catch(err) {
+        console.log(err);
+        res.send("Some error in DB");
+    }
 });
 
+// Create New Post
 app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
 });
-
 app.post("/posts", (req, res) => {
-    let { username, content } = req.body;
-    let id = uuidv4();
-    posts.push({ id, username, content });
-    res.redirect("/posts");
+    let q = "INSERT INTO post(id, username, content)  VALUES (?,?, ?)";
+
+    try {
+        connection.query(q, [id, username, content], (err, resut) => {
+        let id = uuidv4();
+        let { username, content } = req.body;            
+            res.redirect("/posts");
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.send("Some error in DB");
+    }
+       
+});
+
+// Delete Post
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let q = `DELETE FROM post WHERE id = '${id}'`;
+
+   try {
+     connection.query(q, (err, result) => {
+        if (err) throw err;
+        res.redirect("/posts");
+    });
+   } catch(err) {
+    console.log(err);
+    res.send("Some error in DB");
+   }
 });
 
 app.get("/posts/:id", (req, res) => {
